77b96186c3ef697a390e0b03998e0b92044113d4
FrDH <info@frebsite.nl>
2015-05-21 23:29:10 +0200

    small fixes

--- src/js/jquery.dotdotdot.js
+++ src/js/jquery.dotdotdot.js
@@ -1,5 +1,5 @@
 /*
- *	jQuery dotdotdot 1.7.2
+ *	jQuery dotdotdot 1.7.3
  *
  *	Copyright (c) Fred Heusschen
  *	www.frebsite.nl
@@ -341,7 +341,7 @@
 		var isTruncated	= false;
 
 		//	Don't put the ellipsis directly inside these elements
-		var notx = 'a table, thead, tbody, tfoot, tr, col, colgroup, object, embed, param, ol, ul, dl, blockquote, select, optgroup, option, textarea, script, style';
+		var notx = 'a, table, thead, tbody, tfoot, tr, col, colgroup, object, embed, param, ol, ul, dl, blockquote, select, optgroup, option, textarea, script, style';
 
 		//	Don't remove these elements even if they are after the ellipsis
 		var noty = 'script, .dotdotdot-keep';
@@ -356,7 +356,7 @@
 					var e	= this,
 						$e	= $(e);
 
-					if ( typeof e == 'undefined' || ( e.nodeType == 3 && $.trim( e.data ).length == 0 ) )
+					if ( typeof e == 'undefined' )
 					{
 						return true;
 					}
